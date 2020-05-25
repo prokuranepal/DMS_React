fece8c89fe696c6c522e0d3e3c22cec28e511eb3
Prasanga <101890972@student.swin.edu.au>
2020-05-25 16:38:53 +1000

    Added basic layout

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div style={{height: '8vh',
+            backgroundColor: '#1C4E80'}}>
+            Navbar
+        </div>
+    )
+}
--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Navbar from '../../components/Navbar/Navbar'
+import Sidebar from '../../components/Sidebar/Sidebar'
+import classes from './Dashboard.module.css'
 
 const Dashboard = props => {
     return (
-        <div><p>Dashboard</p></div>
+        <div className= {classes.Dashboard}>
+            <Navbar className={classes.Navbar} />
+            <div className={classes.Content}>
+                <Sidebar className={classes.Sidebar} />
+                <div className={classes.View}>
+                    Dashboard
+                </div>
+            </div>
+            
+        </div>
     )
 }
 
