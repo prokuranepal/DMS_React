c361083523be3a4eb57e87c292387d31d1b04fdd
Kishor Ghising <bhfty99@gmail.com>
2020-05-31 14:23:44 +0545

    Create component for Single sidebar items and mapp object to render different menu items

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,69 +1,90 @@
 import React from 'react';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import LocalAirportIcon from '@material-ui/icons/LocalAirport';
-import ControlCameraIcon from '@material-ui/icons/ControlCamera';
-import DnsIcon from '@material-ui/icons/Dns';
-import CloudIcon from '@material-ui/icons/Cloud';
-import ErrorIcon from '@material-ui/icons/Error';
-import EventNoteIcon from '@material-ui/icons/EventNote';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 import classes from './Sidebar.module.css';
+import SidebarItem from './SidebarItem/SidebarItem';
 
-const SideBar = () => (
-  <div className={classes.Sidebar}>
-    <ListItem button>
-      <ListItemIcon>
-        <DashboardIcon />
-      </ListItemIcon>
-      <ListItemText primary="Dashboard" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <LocalAirportIcon />
-      </ListItemIcon>
-      <ListItemText primary="DMS" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <ControlCameraIcon />
-      </ListItemIcon>
-      <ListItemText primary="Drone control" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <DnsIcon />
-      </ListItemIcon>
-      <ListItemText primary="IMS" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <CloudIcon />
-      </ListItemIcon>
-      <ListItemText primary="Weather" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <ErrorIcon />
-      </ListItemIcon>
-      <ListItemText primary="Issue" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <ExitToAppIcon />
-      </ListItemIcon>
-      <ListItemText primary="Event log" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <EventNoteIcon />
-      </ListItemIcon>
-      <ListItemText primary="Mission planner" />
-    </ListItem>
-  </div>
-);
+const SideBar = () => {
+  const SidebarLists = [
+    {
+      "name": "Dashboard",
+      "iconName": "dashboard",
+      "subType": [],
+      "route": "/dashboard"
+    },
+    {
+      "name": "DMS",
+      "iconName": "airplanemode_active",
+      "subType": [],
+      "route": "/dms"
+    },{
+      "name": "Drone control",
+      "iconName": "control_camera",
+      "subType": [],
+      "route": "/dronecontrol"
+    },
+    {
+      "name": "IMS",
+      "iconName": "dns",
+      "subType": [],
+      "route": "/ims"
+    },
+    {
+      "name": "Weather",
+      "iconName": "cloude",
+      "subType": [],
+      "route": "/weather"
+    },
+    {
+      "name": "Issue",
+      "iconName": "error",
+      "subType": [],
+      "route": "/issue"
+    },
+    {
+      "name": "Event log",
+      "iconName": "login",
+      "subType": [],
+      "route": "/eventlog"
+    },
+    {
+      "name": "Mission planner",
+      "iconName": "event_note",
+      "subType": [],
+      "route": "/missionplanner"
+    },
+    {
+      // "name": "Inventory Management System",
+      // "subType": [
+      //     {
+      //         "name": "Add Items",
+      //         "route": "/add"
+      //     },
+      //     {
+      //         "name": "Orders",
+      //         "route": "/orders"
+      //     },
+      // ],
+      // "route": "/ims"
+    }
+  ]
+
+  const menu =
+    SidebarLists.map(SidebarLists => {
+      return (
+        <SidebarItem 
+          iconName={SidebarLists.iconName} 
+          name={SidebarLists.name} 
+          route={SidebarLists.route} 
+          subType={SidebarLists.subType}
+        />
+      );
+    })
+
+  return (
+    <div className={classes.Sidebar}>
+      {menu}
+    </div>
+  );
+};
 
 export default SideBar;
