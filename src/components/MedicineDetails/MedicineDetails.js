2c88f394383dd9c86bd646753c6760ee9e14c943
Kishor Ghising <55025216+KGhising@users.noreply.github.com>
2020-06-22 22:32:00 +0545

    Medicine details (#29)
    * Add testing package jest and enzyme
    
    * Implement basic layout for medicine Details
    
    * Remove import medicineDetails at dashboard to avoid conflict later
    
    Co-authored-by: Swain SHrestha <swainstha@outlook.com>

---
+++ src/components/MedicineDetails/MedicineDetails.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(() => ({
+  modal: {
+    position: 'absolute',
+    top: '40%',
+    left: '40%',
+    width: 400,
+    background: '#fff',
+    borderRadius: '2px',
+    boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
+    textAlign: 'center',
+  },
+  medicineImage: {
+    width: '100%',
+  },
+  editButton: {
+    marginBottom: '20px',
+  },
+}));
+
+const MedicineDetails = (props) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.modal}>
+      <img className={classes.medicineImage} src="https://images.unsplash.com/photo-1471864190281-a93a3070b6de?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" alt="Medicine Image" />
+      <div className={classes.medicineDetails}>
+        <h3>Medicine Name: {props.medicineName}</h3>
+        <h3>Medicine Types: {props.medicineType}</h3>
+        <h3>Medicine Quantity: {props.medicineQuantity}</h3>
+      </div>
+      <Button variant="contained" color="primary" className={classes.editButton}>
+        Edit
+      </Button>
+    </div>
+  );
+}
+
+export default MedicineDetails;
