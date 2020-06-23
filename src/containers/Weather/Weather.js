14543f93a3124d79e3e97cb84127c3d4d61650a4
Swainstha <swainstha@outlook.com>
2020-06-24 02:36:44 +0545

    add weather

--- src/containers/Weather/Weather.js
+++ src/containers/Weather/Weather.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
-
-const useStyles = makeStyles((theme) => ({
+import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+import WeatherItem from './WeatherItem';
+import places from '../../JSONFiles/weather';
+const useStyles = makeStyles({
     root: {
-      display: 'flex',
-      width: '100%',
-      backgroundColor: '#E7E7E7'
+        flexGrow: 1,
+        width: '90%',
+        margin: 'auto',
+        marginTop: '20px'
     },
-    text: {
-        fontSize: 20,
-        margin: 'auto'
+    type: {
+        marginTop: '20px'
     }
-}));
+});
 
-const Weather = props => {
+const Weather = () => {
     const classes = useStyles();
-    return <Grid container className={classes.root} >
-        <Typography className={classes.text}>Weather</Typography>
-    </Grid>
-}
+    // console.log(types);
+    return (
+        <Grid container className={classes.root} spacing={2}>
+            <Grid item xs={12} className={classes.type}>
+                <Grid container spacing={2}>
+                    {places.map(place => {
+                        return <Grid key={place.place} item lg={6} sm={6}>
+                            <WeatherItem place={place.place} temp={place.temp} min={place.min} 
+                            max={place.max} wind={place.wind} humidity={place.humidity} 
+                            description={place.description} image={place.image}
+                            date={place.date}/>
+                        </Grid>
+                    })}
+                </Grid>
+            </Grid>
+        </Grid>
+    );
+};
 
 export default Weather;
