2d74bc2c31e47792ec1e3d6a524b5c5a1f6aaa2a
Swain SHrestha <swainstha@outlook.com>
2020-07-17 18:21:55 +0545

    Modify design of weather (#49)
    * Add Add medicine in IMS
    
    * Add order list
    
    * Add Order List
    
    * Modify Weather design

--- src/app/routes/Weather/WeatherItem.js
+++ src/app/routes/Weather/WeatherItem.js
@@ -1,83 +1,73 @@
-import React from 'react';
-import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import WeatherIcon from './WeatherIcon';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
+import React from 'react'
+// import {weatherData} from './weatherData';
+// import WeatherList from './WeatherList';
+// import Button from '@material-ui/core/Button';
+// import { datePickerDefaultProps } from '@material-ui/pickers/constants/prop-types';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      
-      width: '100%'
-    },
-    content: {
-      flex: '1 0 auto',
-    },
-    cardMedia: {
-      height: 150
-    }
-  }),
-);
+const WeatherDetail = (props) => {
 
-const WeatherItem = props => {
-  const classes = useStyles();
-  let bgColorClass = 'list-weather-widget ';
-  // Set the background colour based on the props.temperature
+  // const {city, list} = weatherData;
+  // if (!city) {
+  //   return <div>Loading...</div>
+  // }
+
+  // TODO: refactoring this code...
+
+  let bgColorClass = 'detail-weather-widget ';
+  // Set the background colour based on the temperature
   if (props.temp >= 30) {
     bgColorClass += 'very-warm';
-  } else if (props.temp > 20 && props.temp < 30) {
+  } else if (props.temp.toFixed(1) > 20 && props.temp.toFixed(1) < 30) {
     bgColorClass += 'warm';
-  } else if (props.temp > 10 && props.temp < 20) {
+  } else if (props.temp.toFixed(1) > 10 && props.temp.toFixed(1) < 20) {
     bgColorClass += 'normal';
-  } else if (props.temp > 0 && props.temp < 10) {
+  } else if (props.temp.toFixed(1) > 0 && props.temp.toFixed(1) < 10) {
     bgColorClass += 'cold';
-  } else if (props.temp <= 0) {
+  } else if (props.temp.toFixed(1) <= 0) {
     bgColorClass += 'very-cold';
   }
+  console.log(bgColorClass);
+  const capitalize = (s) => {
+    if (typeof s !== 'string') return ''
+    const sss = s.split(" ");
+    const d = sss.map(ss => ss.charAt(0).toUpperCase() + ss.slice(1) + " ");
+    return d.join().replace(',', '');
+
+    // s.replace(/(^\w|\s\w)/g, m => m.toUpperCase());
+    // return s;
+  }
+
   return (
-    <Card className={classes.root}>
-      <Grid container >
-        <Grid item container xs={8}>
-          <CardContent className={classes.content}>
-            <Typography component="h6">
-              {props.place}
-            </Typography>
-            <Typography variant="subtitle1" color="textSecondary">
-              {props.date}
-            </Typography>
-            <Divider />
-            <Typography variant="h4">
-              {props.temp}°C
-          </Typography>
-            <Typography variant="caption">
-              {props.min}/{props.max}°C
-          </Typography>
-            <Typography variant="subtitle1">
-              {props.description}
-            </Typography>
-            <Divider />
-            <Typography variant="body2">
-              Wind: {props.wind}mph
-          </Typography>
-            <Typography variant="body2">
-              Humidity: {props.humidity}%
-          </Typography>
-          </CardContent>
-        </Grid>
-        <Grid item container xs={4} alignItems="center">
+    <div className={bgColorClass}>
+      <div className="jr-card-header text-white mt-0">
+        <h2 className="card-heading">{props.place}</h2>
+        <p className="sub-heading">{props.day + ", " + props.time + ", " + capitalize(props.description)}</p>
 
-          <div>
-            <div className="pl-2 pl-md-4">
-              <i className={"text-black jr-fs-xl wi wi-owm-" + props.iconId} />
-            </div>
+        <div className="temp-section">
+          <h2 className="temp-point">{props.temp}
+            <small><sup><sup>°</sup>C</sup></small>
+          </h2>
+          <div className="pl-2 pl-md-4">
+            <i className={"detail-icon wi wi-owm-" + props.iconId} />
           </div>
-        </Grid>
-      </Grid>
-    </Card>
-  );
-}
+        </div>
+        <h4>Humidity:{" " + props.humidity}%</h4>
+        <h3>Wind:{" " + props.wind}mph</h3>
+      </div>
 
-export default WeatherItem;
+      {/* <div className="jr-card-body">
+        <div className={bgColorClass + " mb-4 mb-md-3 row"}>
+          <div className="col-5">
+            <p className="jr-fs-15 mb-0">Humidity</p>
+          </div>
+          <div className="col-4">
+            <p className="mb-0">{props.humidity}%</p>
+          </div>
+
+        </div>
+      </div> */}
+    </div>
+  )
+};
+
+export default WeatherDetail;
