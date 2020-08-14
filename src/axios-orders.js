e14cc3e8704cae5be67c1ca33ce9a14a37ce68b3
Swain SHrestha <swainstha@outlook.com>
2020-08-14 16:23:11 +0545

    Update server endpoints for sign in and dashboard (#59)

---
+++ src/axios-orders.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const url='http://01692a29d8cd.ngrok.io/';
+const instance = axios.create({
+    // baseURL: 'http://35.243.172.120'
+    //  baseURL: 'http://192.168.10.121:8000'
+    // baseURL: 'http://192.168.1.23:8002'
+    //baseURL: 'http://192.168.1.16:8002'
+    // baseURL: 'http://192.168.10.121:8002'
+    baseURL:url
+});
+
+export default instance;
