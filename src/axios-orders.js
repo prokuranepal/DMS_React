7a403f9e11b5d94878877662fc77aa958fb1666f
Swainstha <swainstha@outlook.com>
2020-08-21 15:57:06 +0545

    changed url to env variable

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const url='http://01692a29d8cd.ngrok.io/';
+// const url='http://01692a29d8cd.ngrok.io/';
+const url = process.env.URL;
 const instance = axios.create({
     // baseURL: 'http://35.243.172.120'
     //  baseURL: 'http://192.168.10.121:8000'
