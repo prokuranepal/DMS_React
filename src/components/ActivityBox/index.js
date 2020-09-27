5dc9fc3153c56f16a825bec3897b660be69605d4
sazan10 <070bex432@ioe.edu.np>
2020-09-28 01:44:39 +0545

    testing travis deployment for github page

--- src/components/ActivityBox/index.js
+++ src/components/ActivityBox/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ActivityBoxComponent from './ActivityBoxComponent';
-
+/**
+ * Wraps provided components in a nice box
+ * @returns {ActivityBoxComponent} returns a well styled div component
+ * @param {props} props visit, newVisit, lastWeekVisit, styleName
+ */
 const ActivityBox = ({visit, newVisit, lastWeekVisit, styleName}) => {
   let values= [{title: "Page / Visit", value:visit}, {title:"New Visit",value:newVisit}]
   let content = values.map((val,index)=><ActivityBoxComponent key={index} propTitle={val.title} propValue={val.value}/>)
