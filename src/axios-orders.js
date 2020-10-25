361b4935ee836da57ca90559476d2e2022d32f19
sazan10 <070bex432@ioe.edu.np>
2020-10-25 10:28:08 +0545

    changes ip

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 // const url='http://01692a29d8cd.ngrok.io/';
 // const url = process.env.URL;
-const url ="http://dms.prokurainnovations.com/"
+// const url ="http://dms.prokurainnovations.com/"
+const url ="http://03285eda5508.ngrok.io/"
+
 const instance = axios.create({
     // baseURL: 'http://35.243.172.120'
     //  baseURL: 'http://192.168.10.121:8000'
