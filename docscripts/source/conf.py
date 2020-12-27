fa6ef2be0a92dfd1add8888cf3dc51b0720b3427
Sushil khadka <31089458+Grunze1s@users.noreply.github.com>
2020-12-27 17:10:31 +0545

    Update conf.py

--- docscripts/source/conf.py
+++ docscripts/source/conf.py
@@ -52,7 +52,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['static']
 js_source_path=['../../src','../../src/homeComponents/CheckList','../../src/components/ActivityBox','../../src/components/CardBox','../../src/components/CardMenu','../../src/components/CardLayout','../../src/components/InfoCard']
 root_for_relative_js_paths='../../src'
 primary_domain = 'js'
