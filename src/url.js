ba3b4b07712c290b45d3e8666165809b027b14df
Swainstha <swainstha@outlook.com>
2021-04-02 11:07:10 +0545

    Change url.js and default.conf to change the subdomain to hive.prokurainnovations.com

--- src/url.js
+++ src/url.js
@@ -1,7 +1,7 @@
 // let url = 'http://34f8251071b8.ngrok.io';
-let url='https://dms.prokurainnovations.com:3444'
+let url='https://hive.prokurainnovations.com:3444'
 if (process.env.NODE_ENV === 'production') {
-    url='https://dms.prokurainnovations.com:3444'
+    url='https://hive.prokurainnovations.com:3444'
 }
 
 export default url;
